f3183f053c6b7177a522a52c84dce4428b7b81ea
satya <veeruboppana@gmail.com>
2025-07-07 22:48:21 -0400

    Initial commit with SSH

---
+++ src/main/java/com/parking/slotreservation/SlotreservationApplication.java
@@ -0,0 +1,13 @@
+package com.parking.slotreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlotreservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlotreservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parking/slotreservation/SlotreservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parking.slotreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlotreservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
